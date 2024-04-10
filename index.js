ce8171b7b088565142fa81092195135ff50ac75b
thandomathebe <2483964@students.wits.ac.za>
2024-04-10 12:39:31 +0200

    Update file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
@@ -46,7 +47,7 @@ app.post('/cars', (req, res) => {
     res.json(newCar);
 });
 
-//start app at localhost:3001
-app.listen(3001, () => {
-    console.log('Server started at http://localhost:3001');
+//start app at localhost:3000
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
 });
